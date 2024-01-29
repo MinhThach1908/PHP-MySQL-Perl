25157d7f2fefedc96ba2f7d295f0847e16dc3dcc
MinhThach1908 <pandaminh2008@gmail.com>
2024-01-29 20:17:02 +0700

    chap04

--- chapter04/update.php
+++ chapter04/update.php
@@ -126,7 +126,7 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Create Record</title>
+    <title>Update Record</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         .wrapper{
