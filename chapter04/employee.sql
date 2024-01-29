6b1ef4aadda8d452983706ddd91d98a49a0b0830
MinhThach1908 <pandaminh2008@gmail.com>
2024-01-29 20:30:20 +0700

    chap04

--- chapter04/employee.sql
+++ chapter04/employee.sql
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `employees` (
-  `id` int(11) NOT NULL AUTO_INCREMENT=4 ;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `address` varchar(255) NOT NULL,
   `salary` int(10) NOT NULL,
