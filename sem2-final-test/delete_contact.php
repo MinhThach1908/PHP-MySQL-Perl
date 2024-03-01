16f6d481f3fe16fd6a373e70ea7d0ce97450f523
MinhThach1908 <pandaminh2008@gmail.com>
2024-03-01 20:27:28 +0700

    final test (sem2)

--- sem2-final-test/delete_contact.php
+++ sem2-final-test/delete_contact.php
@@ -1,14 +1,14 @@
 <?php
 
-if(isset($_POST["ID"]) && !empty($_POST["ID"])){
+if(isset($_POST["id"]) && !empty($_POST["id"])){
     require_once 'config.php';
 
-    $sql = "DELETE FROM contacts WHERE id = ?";
+    $sql = "DELETE FROM students WHERE id = ?";
 
     if($stmt = mysqli_prepare($link, $sql)){
         mysqli_stmt_bind_param($stmt, "i", $param_id);
 
-        $param_id = trim($_POST["ID"]);
+        $param_id = trim($_POST["id"]);
 
         if(mysqli_stmt_execute($stmt)){
             header("location: index.php");
@@ -22,7 +22,7 @@ if(isset($_POST["ID"]) && !empty($_POST["ID"])){
 
     mysqli_close($link);
 } else{
-    if(empty(trim($_GET["ID"]))){
+    if(empty(trim($_GET["id"]))){
         header("location: error.php");
         exit();
     }
@@ -51,7 +51,7 @@ if(isset($_POST["ID"]) && !empty($_POST["ID"])){
                 </div>
                 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                     <div class="alert alert-danger fade in">
-                        <input type="hidden" name="ID" value="<?php echo trim($_GET["ID"]); ?>"/>
+                        <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
                         <p>Are you sure you want to delete this contact?</p><br>
                         <p>
                             <input type="submit" value="Yes" class="btn btn-danger">
